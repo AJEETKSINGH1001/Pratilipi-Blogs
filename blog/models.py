608412f52254fa8767e0710b5487b76febd85436
AJEETKSINGH1001 <ajeet10jan@gmail.com>
2023-12-11 19:19:50 +0530

    Pratilipi blogs site first commit

---
+++ blog/models.py
@@ -0,0 +1,26 @@
+# blog/models.py
+
+from django.db import models
+from django.urls import reverse
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to='post_images/', null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('post_detail', args=[str(self.id)])
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.CharField(max_length=100)
+    text = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.text
+
---
+++ blog/urls.py
@@ -0,0 +1,12 @@
+# blog/urls.py
+
+from django.urls import path
+from . import views
+
+app_name = 'blog'
+
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.create_post, name='create_post'),  # New URL pattern for creating a post
+]
